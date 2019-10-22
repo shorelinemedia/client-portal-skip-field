d8b20f30a61e7c95eb2598695ec27f07e37bc6b9
Chris Matthias <chrism@shorelinemedianj.com>
2019-10-22 14:14:59 -0400

    adding custom WC template for the 'orderby' filter dropdown; updated readme to be able to remove this custom template

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ In some cases, your form HTML might be manipulated with a filter, like the nativ
 If you need to target additional HTML to replace with the hidden skip field, you can customize the matching pattern to match particular form HTML with the filter `sl9_skip_field_pattern`.
 
 If you need to update the hidden field markup (only use at your own risk), use filter `sl9_skip_field_input`. Use the <a href="https://github.com/afragen/github-updater" target="_blank">Github Updater</a> plugin to update this plugin in the future.
+
+### Templates
+
+Some HTML forms do not expose a filter or action to be able to manipulate form HTML on-the-fly. We use custom templates for some of these cases, like the WooCommerce 'orderby' filter dropdown.
+
+If you have a custom template being used in your theme or plugin and you don't want us to serve our WooCommerce templates, in your theme simply use:
+
+``
+remove_filter( 'woocommerce_locate_template',  'sl9_client_portal_woocommerce_locate_template', 10, 3 );
+``
+
+If you would like to move our filter to a higher or lower priority, simply remove the filter as above and then use `add_filter` to reinstate the filter with your own priority.
--- client-portal-skip-field.php
+++ client-portal-skip-field.php
@@ -52,3 +52,43 @@ add_action( 'gmw_search_form_end', 'sl9_client_portal_insert_hidden_skip_field' 
 add_action( 'fb_popup_button', 'sl9_client_portal_insert_hidden_skip_field' );
 // Add to KLEO register modal
 add_action( 'fb_popup_register_button', 'sl9_client_portal_insert_hidden_skip_field' );
+
+/**
+ * Use custom WooCommerce templates where the form HTML is not accessible via filter or hook
+ */
+
+function sl9_client_portal_plugin_path() {
+  // gets the absolute path to this plugin directory
+  return untrailingslashit( plugin_dir_path( __FILE__ ) );
+}
+
+function sl9_client_portal_woocommerce_locate_template( $template, $template_name, $template_path ) {
+  global $woocommerce;
+
+  $_template = $template;
+
+  if ( ! $template_path ) $template_path = $woocommerce->template_url;
+
+  $plugin_path  = sl9_client_portal_plugin_path() . '/woocommerce/';
+
+  // Look within passed path within the theme - this is priority
+  $template = locate_template(
+
+    array(
+      $template_path . $template_name,
+      $template_name
+    )
+  );
+
+  // Modification: Get the template from this plugin, if it exists
+  if ( ! $template && file_exists( $plugin_path . $template_name ) )
+    $template = $plugin_path . $template_name;
+
+  // Use default template
+  if ( ! $template )
+    $template = $_template;
+
+  // Return what we found
+  return $template;
+}
+add_filter( 'woocommerce_locate_template', 'sl9_client_portal_woocommerce_locate_template', 10, 3 );
